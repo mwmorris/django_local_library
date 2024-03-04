c7b0a9f4382171a1b5d0034f8ea8ed0c04a4f748
Meirion Morris <meirion.morris@btinternet.com>
2024-03-04 17:21:45 +0000

    modified locallibrary/locallibrary/settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6%*#s8mepv75i)r-c@l$j&=dj-mzf=gz*fv40_md(_+qd41!#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['meirionmorris.uk']
+ALLOWED_HOSTS = ['meirionmorris.uk', 'localhost']
 
 
 # Application definition
